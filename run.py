d225d6d748c6566c7f112688d85e4ed49f1483bf
Ian Miell <ian.miell@gmail.com>
2021-11-28 09:19:55 +0000

    end

--- run.py
+++ run.py
@@ -45,4 +45,4 @@ def run(shutit_sessions, machines):
 	#crossplane.run(shutit_sessions, machines)
 	#kube_monkey.run(shutit_sessions, machines)
 	#shell_operator.run(shutit_sessions, machines)
-
+	shutit_pause_point('END OF ALL MODULES')

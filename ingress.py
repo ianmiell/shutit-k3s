b8a0eaba7c9d0ec286ae1427c86a4f04553810ad
Ian Miell <ian.miell@gmail.com>
2021-12-03 14:47:03 +0000

    lates

--- ingress.py
+++ ingress.py
@@ -1,5 +1,15 @@
 def run(shutit_sessions, machines):
 	shutit_session = shutit_sessions['machine1']
+	# TODO: https://stackoverflow.com/questions/56915354/how-to-install-nginx-ingress-with-hostnetwork-on-bare-metal
+	# helm install stable/nginx-ingress --set controller.hostNetwork=true,controller.service.type="",controller.kind=DaemonSet
+	# In deployments:
+	# spec:
+	#  template:
+	#    spec:
+	#      hostNetwork: true
+	# metadata:
+	#  annotations:
+	#    kubernetes.io/ingress.class: "nginx"
 	shutit_session.send('helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx')
 	shutit_session.send('helm repo update')
 	shutit_session.send('''cat > values.yaml << EOF

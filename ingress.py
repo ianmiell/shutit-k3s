057ba997b0b27239468a30a2d4b1b492379f9c77
Ian Miell <ian.miell@gmail.com>
2021-12-03 14:36:22 +0000

    ingress

--- ingress.py
+++ ingress.py
@@ -1,16 +1,15 @@
 def run(shutit_sessions, machines):
 	shutit_session = shutit_sessions['machine1']
-
-#helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-#helm repo update
-#cat > values.yaml << EOF
-#controller:
-#  service:
-#    type: NodePort
-#    nodePorts:
-#      http: 32080
-#      https: 32443
-#      tcp:
-#        8080: 32808
-#EOF
-#helm install ingress-nginx/ingress-nginx  --generate-name --values values.yaml
+	shutit_session.send('helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx')
+	shutit_session.send('helm repo update')
+	shutit_session.send('''cat > values.yaml << EOF
+controller:
+  service:
+    type: NodePort
+    nodePorts:
+      http: 32080
+      https: 32443
+      tcp:
+        8080: 32808
+EOF''')
+	shutit_session.send('helm install ingress-nginx/ingress-nginx --generate-name --values values.yaml')

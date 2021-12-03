062f257413d0e60d8ce1c8fdb93d562023f545af
Ian Miell <ian.miell@gmail.com>
2021-12-03 14:34:34 +0000

    latest

--- run.py
+++ run.py
@@ -86,6 +86,8 @@ def run(shutit_sessions, machines):
 	#kube_monkey.run(shutit_sessions, machines)
 	#kubevirt.run(shutit_sessions, machines)
 	#mutating_webhook.run(shutit_sessions, machines)
+	#ingress.run(shutit_sessions, machines)
+	#kube_monkey.run(shutit_sessions, machines)
 	#rook.run(shutit_sessions, machines)
 	#shell_operator.run(shutit_sessions, machines)
 	shutit_session.pause_point('END OF ALL MODULES')

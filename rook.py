308fd462e80e8b8900b38ed14d00b72f2fe44c78
Ian Miell <ian.miell@gmail.com>
2022-01-16 13:44:15 +0000

    latest

--- rook.py
+++ rook.py
@@ -90,17 +90,6 @@ spec:
 EOF
 )''')
 	shutit_session.send_until("kubectl get cephcluster -n rook-ceph -o json | jq '.items[0].status.phase'", '"Ready"', cadence=30)
-=======
-   23  git clone --single-branch --branch v1.7.8 https://github.com/rook/rook.git
-   24  cd rook/cluster/examples/kubernetes/ceph
-   25  kubectl create -f crds.yaml -f common.yaml -f operator.yaml
-   26  kubectl create -f cluster.yaml
->>>>>>> 8472f3e... helm
-=======
-	shutit_session.send('cd rook/cluster/examples/kubernetes/ceph')
-	shutit_session.send('kubectl create -f crds.yaml -f common.yaml -f operator.yaml')
-	shutit_session.send('kubectl create -f cluster.yaml')
->>>>>>> 7210fef... latest
 	# Create the toolbox
 	shutit_session.send('''kubectl create -f <(cat << EOF
 apiVersion: apps/v1
@@ -158,8 +147,6 @@ spec:
           effect: "NoExecute"
           tolerationSeconds: 5
 EOF
-<<<<<<< HEAD
-<<<<<<< HEAD
 )''')
 	shutit_session.send_until('kubectl -n rook-ceph rollout status deploy/rook-ceph-tools | grep successfully.rolled.out | wc -l', '1')
 	shutit_session.login(command='kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- bash')

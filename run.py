e5264ab575956e407ed1b68b3ff2b637ebe09def
Ian Miell <ian.miell@gmail.com>
2022-01-23 08:56:40 +0000

    crossplan

--- run.py
+++ run.py
@@ -79,7 +79,7 @@ def run(shutit_sessions, machines):
 	import rook
 	import shell_operator
 	#admission_controller.run(shutit_sessions, machines)
-	#crossplane.run(shutit_sessions, machines)
+	crossplane.run(shutit_sessions, machines)
 	#istio_in_action.run(shutit_sessions, machines)
 	#ingress.run(shutit_sessions, machines)
 	#istio_egress.run(shutit_sessions, machines)

c6e736781e5df4e3e2b903898988e49154f3c769
Ian Miell <ian.miell@gmail.com>
2022-03-31 15:49:09 +0100

    latest

--- admission_controller.py
+++ admission_controller.py
@@ -51,77 +51,76 @@ spec:
         patchStrategicMerge:
           automountServiceAccountToken: false
 EOF''')
+	# Give time for the policy to take effect
+	shutit_session.send('sleep 60')
 	shutit_session.send('kubectl create ns blahse')
 	shutit_session.send('kubectl get sa -o yaml blahse | grep automount')
 	shutit_session.send('kubectl create ns b2')
-	shutit_session.send('kubectl get sa -o yaml b2 | grep automount')
+	# This should not have an automountServiceAccountToken item in it
+	shutit_session.send('kubectl get sa -o yaml b2')
+	shutit_session.pause_point('')
 
 
-
+	##### OLD NOTES
 	## This has been suggested:
 	##https://github.com/viveksinghggits/valkontroller/
 	#shutit_session.send('git clone https://github.com/viveksinghggits/valkontroller/')
 	#shutit_session.send('cd valkontroller')
 
-
-
 	# Do we really need to go into all that trouble to modify Kubernetes objects as they are created? Not really, there are generic tools to do so like the KubeMod operator
 	# https://github.com/kubemod/kubemod
 
+	### UNFINISHED ATTEMPT
+	# https://trstringer.com/kubernetes-mutating-webhook/
+	#shutit_session.send('git clone https://github.com/trstringer/kubernetes-mutating-webhook')
+	#shutit_session.send('cd kubernetes-mutating-webhook')
 
+	### OUTDATED PROJECT, github 404s
+	#https://slack.engineering/simple-kubernetes-webhook/
 
+	### OUTDATED PROJECT
+	#https://medium.com/ovni/writing-a-very-basic-kubernetes-mutating-admission-webhook-398dbbcb63ec
+	#shutit_session.send('git clone https://github.com/alex-leonhardt/k8s-mutate-webhook')
+	#ca_bundle = shutit_session.send_and_get_output("kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}'")
 
-### UNFINISHED
-# https://trstringer.com/kubernetes-mutating-webhook/
-#shutit_session.send('git clone https://github.com/trstringer/kubernetes-mutating-webhook')
-#shutit_session.send('cd kubernetes-mutating-webhook')
+	### OUTDATED PROJECT
+	# From: https://docs.giantswarm.io/advanced/custom-admission-controller/
+	#	shutit_session.send('git clone https://github.com/giantswarm/grumpy')
+	#	shutit_session.send('cd grumpy')
+	#	shutit_session.pause_point('ready')
 
-### OUTDATED github 404s
-#https://slack.engineering/simple-kubernetes-webhook/
-
-### OUTDATED
-#https://medium.com/ovni/writing-a-very-basic-kubernetes-mutating-admission-webhook-398dbbcb63ec
-#shutit_session.send('git clone https://github.com/alex-leonhardt/k8s-mutate-webhook')
-#ca_bundle = shutit_session.send_and_get_output("kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}'")
-
-### OUTDATED
-# From: https://docs.giantswarm.io/advanced/custom-admission-controller/
-#	shutit_session.send('git clone https://github.com/giantswarm/grumpy')
-#	shutit_session.send('cd grumpy')
-#	shutit_session.pause_point('ready')
-
-##Load this in:
-#apiVersion: admissionregistration.k8s.io/v1beta1
-#kind: ValidatingWebhookConfiguration
-#metadata:
-#  name: grumpy
-#webhooks:
-#  - name: grumpy
-#    clientConfig:
-#      service:
-#        name: grumpy
-#        namespace: default
-#        path: "/validate"
-#      caBundle: "${CA_BUNDLE}"
-#    rules:
-#      - operations: ["CREATE"]
-#        apiGroups: [""]
-#        apiVersions: ["v1"]
-#        resources: ["pods"]
-#
-##From this repo:
-#git clone https://github.com/giantswarm/grumpy
-#
-##run:
-#https://github.com/giantswarm/grumpy/blob/instance_migration/gen_certs.sh
-#
-## Store as secret
-#kubectl create secret generic grumpy -n default --from-file=key.pem=certs/grumpy-key.pem --from-file=cert.pem=certs/grumpy-crt.pem
-#
-## Apply manifest
-#https://github.com/giantswarm/grumpy/blob/instance_migration/manifest.yaml
-#
-## Run the pod
-#smooth-app.yml
+	##Load this in:
+	#apiVersion: admissionregistration.k8s.io/v1beta1
+	#kind: ValidatingWebhookConfiguration
+	#metadata:
+	#  name: grumpy
+	#webhooks:
+	#  - name: grumpy
+	#    clientConfig:
+	#      service:
+	#        name: grumpy
+	#        namespace: default
+	#        path: "/validate"
+	#      caBundle: "${CA_BUNDLE}"
+	#    rules:
+	#      - operations: ["CREATE"]
+	#        apiGroups: [""]
+	#        apiVersions: ["v1"]
+	#        resources: ["pods"]
+	#
+	##From this repo:
+	#git clone https://github.com/giantswarm/grumpy
+	#
+	##run:
+	#https://github.com/giantswarm/grumpy/blob/instance_migration/gen_certs.sh
+	#
+	## Store as secret
+	#kubectl create secret generic grumpy -n default --from-file=key.pem=certs/grumpy-key.pem --from-file=cert.pem=certs/grumpy-crt.pem
+	#
+	## Apply manifest
+	#https://github.com/giantswarm/grumpy/blob/instance_migration/manifest.yaml
+	#
+	## Run the pod
+	#smooth-app.yml
 
 
